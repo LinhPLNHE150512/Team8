b0bcec086661e5e7b62bf3b661a0f3c860914afc
Pham Le Nhat Linh <phamlenhatlinh22@gmail.com>
2022-04-25 09:22:34 +0700

    click here

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './weather.model';
+import { WeatherService } from './weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  title = 'Weather_App_Project';
+
+  constructor(private weatherService: WeatherService) { }
+
+  weatherData?: WeatherData
+  cityName: string = 'London';
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string){
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response);
+        }
+      });
+  }
+}
